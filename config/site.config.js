a98be147f76f0194c90838d05ee427f2674b6ebf
Shailesh Chaudhary <shaileshchaudhary9925@gmail.com>
2023-02-03 23:31:52 +0530

    first commit

---
+++ config/site.config.js
@@ -0,0 +1,117 @@
+const siteConfig = {
+  seo: {
+    title: 'The World News',
+    author: 'Theworldnews',
+    copyright: 'Theworldnews',
+    description:
+      'News update online, latest on current happenings and interesting global information,news, as well as business news.',
+    image:
+      'https://res.cloudinary.com/theworldnews/image/upload/v1675446602/website/Theworldnews.png',
+    altText: 'Theworldnews',
+    ogType: 'website',
+    monetizationTag: '',
+    twitterUsername: '@',
+    pages: {
+      supportMe: {
+        title: 'Support Me | Theworldnews',
+        description:
+          'Your support keeps this Site alive and motivates me to keep creating content.',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446603/website/Support-Theworldnews.png'
+      },
+      articles: {
+        title: 'Articles | Theworldnews',
+        description:
+          'Read articles on news and global information',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446602/website/Articles-Theworldnews.png'
+      },
+      snippets: {
+        title: 'Snippets | Theworldnews',
+        description:
+          'Breaking news, headlines and top stories around the globe.',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446603/website/Snippets-Theworldnews.png'
+      },
+      categories: {
+        title: 'Categories | Theworldnews',
+        description: 'Browse different categories.',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446603/website/Categories-Theworldnews.png'
+      },
+      tags: {
+        title: 'Tags | Theworldnews',
+        description: 'Explore all the tags.',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446603/website/Tags-Theworldnews.png'
+      },
+      privacy: {
+        title: 'Privacy Policy | Theworldnews',
+        description: 'This page contains Theworldnews privacy policies.',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446602/website/Privacy-Theworldnews.png'
+      },
+      cookie: {
+        title: 'Cookie Policy | Theworldnews',
+        description: 'This page contains Theworldnews cookie policies.',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446602/website/Cookie-Theworldnews.png'
+      },
+      terms: {
+        title: 'Terms & Conditions | Theworldnews',
+        description: 'This page contains Theworldnews terms & conditions.',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446602/website/Terms-Theworldnews.png'
+      },
+      externalArticles: {
+        title: 'External Articles | Theworldnews',
+        description: 'Read articles written by Theworldnews on other sites.',
+        image:
+          'https://res.cloudinary.com/theworldnews/image/upload/v1675446602/website/External-Theworldnews.png'
+      }
+    }
+  },
+  branding: {
+    logo: 'https://res.cloudinary.com/theworldnews/image/upload/v1675446843/website/Round_logo_The_world_news.png',
+    colors: {
+      brand: {
+        50: '#7868E6',
+        100: '#FF5403',
+        200: '#FF2E63',
+        300: '#FFCC00',
+        400: '#48466D',
+        500: '#769FCD'
+      },
+      text: '#454E56',
+      themeLight: '#7868E6',
+      themeDark: '#000000'
+    },
+    fonts: {
+      heading: 'Alliance No 2',
+      body: 'Poppins'
+    }
+  },
+  urls: {
+    socials: {
+      twitter: 'https://twitter.com/Theworldnews',
+      linkedin: 'https://www.linkedin.com/in/Theworldnews',
+      github: 'https://github.com/Theworldnews',
+      showwcase: ''
+    },
+    about: '',
+    status: '',
+    newsletter: '',
+    kofi: 'https://ko-fi.com/'
+  },
+  adsense: {
+    publisherId: '',
+    slots: {
+      pageTop: '',
+      inFeed: ''
+    }
+  },
+  copyrightText: `© ${new Date().getFullYear()} Theworldnews. All rights reserved.`,
+  siteVersion: '1.1.0'
+};
+
+export default siteConfig;
