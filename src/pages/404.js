4d72e37caa9e6a212edc3c9529dceb8df95c4ed3
Shailesh Chaudhary <57599212+shaileshofficial2019@users.noreply.github.com>
2023-02-13 22:55:29 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,6 +2,7 @@ import { Analytics, SEO } from '../common/utils';
 import Navbar from '@/components/navbar/Navbar';
 import { Newsletter } from '@/components/misc';
 import Footer from '@/components/footer/Footer';
+import Link from 'next/link'
 
 const FourOhFour = () => {
   return (
@@ -11,7 +12,7 @@ const FourOhFour = () => {
 
       <Navbar />
       <main>
-        <center> 404 <br /> <h3> Not Found ! </h3> <br /> <a href='/'> Go Home</a></center>
+        <center> 404 <br /> <h3> Not Found ! </h3> <br /> <Link href='/'> Go Home</Link></center>
         <Newsletter />
       </main>
       <Footer />
