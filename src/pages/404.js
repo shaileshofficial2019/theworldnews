4246f050572111c211c1eb07a3b6e140b561d576
Shailesh Chaudhary <57599212+shaileshofficial2019@users.noreply.github.com>
2023-02-13 22:52:03 +0530

    Create 404.js

---
+++ src/pages/404.js
@@ -0,0 +1,23 @@
+import { Analytics, SEO } from '../common/utils';
+import Navbar from '@/components/navbar/Navbar';
+import { Newsletter } from '@/components/misc';
+import Footer from '@/components/footer/Footer';
+
+const FourOhFour = () => {
+  return (
+    <>
+      <SEO />
+      <Analytics />
+
+      <Navbar />
+      <main>
+        <center> 404 <br> <h3> Not Found ! </h3> <br> <a href='/'> Go Home</a></center>
+        <Newsletter />
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+
+export default FourOhFour;
